33fef794416acb925ed8becbeb76b533b52b4863
bstore <bstore@users.noreply.github.com>
2017-04-01 16:12:02 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1QccWSoxgUEnqLz62iRxOmVGebsPvLgKkJp7Cme3Fnk0/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1QccWSoxgUEnqLz62iRxOmVGebsPvLgKkJp7Cme3Fnk0/edit#gid=0 ';
